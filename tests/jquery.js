32b3d55ec34ee618ccf86b9406087adc1320b5fb
Ger Hobbelt <ger@hobbelt.com>
2013-08-24 22:12:13 +0200

    - remove non-existent file 'tabs/tabs_deprecated.html' from the test set 'all' - it had been removed in a previous commit already.
    - fix URIs pointing at the jquery.js loader code.
    
    - updated qUnit to the bleeding edge

--- tests/jquery.js
+++ tests/jquery.js
@@ -1,11 +1,14 @@
-(function() {
+(function(window) {
+
+// OPTIONAL global var: jquery_script_basedir
 
 var parts = document.location.search.slice( 1 ).split( "&" ),
 	length = parts.length,
 	i = 0,
 	current,
 	version,
-	url;
+	url,
+	path;
 
 for ( ; i < length; i++ ) {
 	current = parts[ i ].split( "=" );
@@ -18,9 +21,42 @@ for ( ; i < length; i++ ) {
 if ( version === "git" ) {
 	url = "http://code.jquery.com/jquery-git.js";
 } else {
-	url = "../../jquery-" + ( version || "bleeding-edge" ) + ".js";
+	path = (window.jquery_script_basedir || "../..");
+	url = path + "/jquery-" + ( version || "bleeding-edge" ) + ".js";
 }
 
 document.write( "<script src='" + url + "'></script>" );
 
-}() );
+window.onJqueryLoaded = function() {
+	var queue = [];
+	var h;
+	var check_if_loaded = function() {
+		if (h) clearTimeout(h);
+		h = null;
+
+		if (window.jQuery) {
+			var f;
+
+			// written so that f() can invoke window.onJqueryLoaded(f) itself without damaging the process:
+			while (queue.length > 0) {
+				f = queue.shift();
+				f();
+			}
+			return true;
+		} else {
+			// jQuery has not loaded yet: try again in a little while (POLLING):
+			h = setTimeout(check_if_loaded, 100);
+			return false;
+		}
+	};
+
+	return function(f) {
+		if (f) queue.push(f);
+
+		return check_if_loaded();
+	}
+};
+
+window.onJqueryLoaded = window.onJqueryLoaded();
+
+}(this) );
