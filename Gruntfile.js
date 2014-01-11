3337c9022fcd51d8877183d54e840d170e6afd23
Ger Hobbelt <ger@hobbelt.com>
2014-01-12 00:28:43 +0100

    fix merge mistake: make Gruntfile.js compatible with latest Grunt tool

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,11 +18,11 @@ var
 
 	uiFiles = coreFiles.map(function( file ) {
 		return "ui/" + file;
-	}).concat( grunt.file.expandFiles( "ui/*.js" ).filter(function( file ) {
+	}).concat( expandFiles( "ui/*.js" ).filter(function( file ) {
 		return coreFiles.indexOf( file.substring( 3 ) ) === -1;
 	}) ),
 
-	allI18nFiles = grunt.file.expandFiles( "ui/i18n/*.js" ),
+	allI18nFiles = expandFiles( "ui/i18n/*.js" ),
 
 	cssFiles = [
 		"core",
@@ -49,12 +49,25 @@ var
 
 	// minified files
 	minify = {
-		"dist/jquery-ui.min.js": [ "<banner:meta.bannerAll>", "dist/jquery-ui.js" ],
-		"dist/i18n/jquery-ui-i18n.min.js": [ "<banner:meta.bannerI18n>", "dist/i18n/jquery-ui-i18n.js" ]
-	},
-
-	minifyCSS = {
-		"dist/jquery-ui.min.css": "dist/jquery-ui.css"
+		options: {
+			preserveComments: false
+		},
+		main: {
+			options: {
+				banner: createBanner( uiFiles )
+			},
+			files: {
+				"dist/jquery-ui.min.js": "dist/jquery-ui.js"
+			}
+		},
+		i18n: {
+			options: {
+				banner: createBanner( allI18nFiles )
+			},
+			files: {
+				"dist/i18n/jquery-ui-i18n.min.js": "dist/i18n/jquery-ui-i18n.js"
+			}
+		}
 	},
 
 	compareFiles = {
@@ -68,16 +81,25 @@ function mapMinFile( file ) {
 	return "dist/" + file.replace( /\.js$/, ".min.js" ).replace( /ui\//, "minified/" );
 }
 
-uiFiles.concat( allI18nFiles ).forEach(function( file ) {
-	minify[ mapMinFile( file ) ] = [ "<banner>", file ];
-});
+function expandFiles( files ) {
+	return grunt.util._.pluck( grunt.file.expandMapping( files ), "src" ).map(function( values ) {
+		return values[ 0 ];
+	});
+}
 
-cssFiles.forEach(function( file ) {
-	minifyCSS[ "dist/" + file.replace( /\.css$/, ".min.css" ).replace( /themes\/base\//, "themes/base/minified/" ) ] = [ "<banner>", "<strip_all_banners:" + file + ">" ];
+uiFiles.concat( allI18nFiles ).forEach(function( file ) {
+	minify[ file ] = {
+		options: {
+			banner: createBanner()
+		},
+		files: {}
+	};
+	minify[ file ].files[ mapMinFile( file ) ] = file;
 });
 
 uiFiles.forEach(function( file ) {
-	compareFiles[ file ] = [ file,  mapMinFile( file ) ];
+	// TODO this doesn't do anything until https://github.com/rwldrn/grunt-compare-size/issues/13
+	compareFiles[ file ] = [ file, mapMinFile( file ) ];
 });
 
 // grunt plugins
@@ -90,66 +112,58 @@ grunt.loadNpmTasks( "grunt-jscs-checker" );
 grunt.loadNpmTasks( "grunt-css" );
 grunt.loadNpmTasks( "grunt-html" );
 grunt.loadNpmTasks( "grunt-compare-size" );
-//grunt.loadNpmTasks( "grunt-junit" );
 grunt.loadNpmTasks( "grunt-git-authors" );
 grunt.loadNpmTasks( "grunt-esformatter" );
 // local testswarm and build tasks
 grunt.loadTasks( "build/tasks" );
 
-grunt.registerHelper( "strip_all_banners", function( filepath ) {
-	return grunt.file.read( filepath ).replace( /^\s*\/\*[\s\S]*?\*\/\s*/g, "" );
-});
-
-function stripBanner( files ) {
-	return files.map(function( file ) {
-		return "<strip_all_banners:" + file + ">";
-	});
-}
-
 function stripDirectory( file ) {
-	// TODO: we're receiving the directive, so we need to strip the trailing >
-	// we should be receving a clean path without the directive
 	return file.replace( /.+\/(.+?)>?$/, "$1" );
 }
-// allow access from banner template
-global.stripDirectory = stripDirectory;
 
 function createBanner( files ) {
 	// strip folders
 	var fileNames = files && files.map( stripDirectory );
 	return "/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - " +
 		"<%= grunt.template.today('isoDate') %>\n" +
-		"<%= pkg.homepage ? '* ' + pkg.homepage + '\n' : '' %>" +
-		"* Includes: " + (files ? fileNames.join(", ") : "<%= stripDirectory(grunt.task.current.file.src[1]) %>") + "\n" +
+		"<%= pkg.homepage ? '* ' + pkg.homepage + '\\n' : '' %>" +
+		(files ? "* Includes: " + fileNames.join(", ") + "\n" : "") +
 		"* Copyright <%= grunt.template.today('yyyy') %> <%= pkg.author.name %>;" +
-		" Licensed <%= _.pluck(pkg.licenses, 'type').join(', ') %> */";
+		" Licensed <%= _.pluck(pkg.licenses, 'type').join(', ') %> */\n";
 }
 
 grunt.initConfig({
-	pkg: "<json:package.json>",
+	pkg: grunt.file.readJSON( "package.json" ),
 	files: {
-		dist: "<%= pkg.name %>-<%= pkg.version %>",
-		cdn: "<%= pkg.name %>-<%= pkg.version %>-cdn",
-		themes: "<%= pkg.name %>-themes-<%= pkg.version %>"
-	},
-	meta: {
-		banner: createBanner(),
-		bannerAll: createBanner( uiFiles ),
-		bannerI18n: createBanner( allI18nFiles ),
-		bannerCSS: createBanner( cssFiles )
+		dist: "<%= pkg.name %>-<%= pkg.version %>"
 	},
 	compare_size: compareFiles,
 	concat: {
 		ui: {
-			src: [ "<banner:meta.bannerAll>", stripBanner( uiFiles ) ],
+			options: {
+				banner: createBanner( uiFiles ),
+				stripBanners: {
+					block: true
+				}
+			},
+			src: uiFiles,
 			dest: "dist/jquery-ui.js"
 		},
 		i18n: {
-			src: [ "<banner:meta.bannerI18n>", allI18nFiles ],
+			options: {
+				banner: createBanner( allI18nFiles )
+			},
+			src: allI18nFiles,
 			dest: "dist/i18n/jquery-ui-i18n.js"
 		},
 		css: {
-			src: [ "<banner:meta.bannerCSS>", stripBanner( cssFiles ) ],
+			options: {
+				banner: createBanner( cssFiles ),
+				stripBanners: {
+					block: true
+				}
+			},
+			src: cssFiles,
 			dest: "dist/jquery-ui.css"
 		}
 	},
@@ -160,151 +174,19 @@ grunt.initConfig({
 		// tests: "tests/unit/**/*.js",
 		grunt: "Gruntfile.js"
 	},
-	min: minify,
-	cssmin: minifyCSS,
+	uglify: minify,
 	htmllint: {
 		// ignore files that contain invalid html, used only for ajax content testing
 		all: grunt.file.expand( [ "demos/**/*.html", "tests/**/*.html" ] ).filter(function( file ) {
 			return !/(?:ajax\/content\d\.html|tabs\/data\/test\.html|tests\/unit\/core\/core.*\.html)/.test( file );
 		})
 	},
-	copy: {
-		dist: {
-			src: [
-				"AUTHORS.txt",
-				"jquery-*.js",
-				"MIT-LICENSE.txt",
-				"README.md",
-				"grunt.js",
-				"package.json",
-				"*.jquery.json",
-				"ui/**/*",
-				"ui/.jshintrc",
-				"demos/**/*",
-				"themes/**/*",
-				"external/**/*",
-				"tests/**/*"
-			],
-			renames: {
-				"dist/jquery-ui.js": "ui/jquery-ui.js",
-				"dist/jquery-ui.min.js": "ui/minified/jquery-ui.min.js",
-				"dist/i18n/jquery-ui-i18n.js": "ui/i18n/jquery-ui-i18n.js",
-				"dist/i18n/jquery-ui-i18n.min.js": "ui/minified/i18n/jquery-ui-i18n.min.js",
-				"dist/jquery-ui.css": "themes/base/jquery-ui.css",
-				"dist/jquery-ui.min.css": "themes/base/minified/jquery-ui.min.css"
-			},
-			dest: "dist/<%= files.dist %>"
-		},
-		dist_min: {
-			src: "dist/minified/**/*",
-			strip: /^dist/,
-			dest: "dist/<%= files.dist %>/ui"
-		},
-		dist_css_min: {
-			src: "dist/themes/base/minified/*.css",
-			strip: /^dist/,
-			dest: "dist/<%= files.dist %>"
-		},
-		dist_units_images: {
-			src: "themes/base/images/*",
-			strip: /^themes\/base\//,
-			dest: "dist/"
-		},
-		dist_min_images: {
-			src: "themes/base/images/*",
-			strip: /^themes\/base\//,
-			dest: "dist/<%= files.dist %>/themes/base/minified"
-		},
-		cdn: {
-			src: [
-				"AUTHORS.txt",
-				"MIT-LICENSE.txt",
-				"ui/*.js",
-				"package.json"
-			],
-			renames: {
-				"dist/jquery-ui.js": "jquery-ui.js",
-				"dist/jquery-ui.min.js": "jquery-ui.min.js",
-				"dist/i18n/jquery-ui-i18n.js": "i18n/jquery-ui-i18n.js",
-				"dist/i18n/jquery-ui-i18n.min.js": "i18n/jquery-ui-i18n.min.js",
-				"dist/jquery-ui.css": "themes/base/jquery-ui.css",
-				"dist/jquery-ui.min.css": "themes/base/minified/jquery-ui.min.css"
-			},
-			dest: "dist/<%= files.cdn %>"
-		},
-		cdn_i18n: {
-			src: "ui/i18n/jquery.ui.datepicker-*.js",
-			strip: "ui/",
-			dest: "dist/<%= files.cdn %>"
-		},
-		cdn_i18n_min: {
-			src: "dist/minified/i18n/jquery.ui.datepicker-*.js",
-			strip: "dist/minified",
-			dest: "dist/<%= files.cdn %>"
-		},
-		cdn_min: {
-			src: "dist/minified/*.js",
-			strip: /^dist\/minified/,
-			dest: "dist/<%= files.cdn %>/ui"
-		},
-		cdn_min_images: {
-			src: "themes/base/images/*",
-			strip: /^themes\/base\//,
-			dest: "dist/<%= files.cdn %>/themes/base/minified"
-		},
-		cdn_themes: {
-			src: "dist/<%= files.themes %>/themes/**/*",
-			strip: "dist/<%= files.themes %>",
-			dest: "dist/<%= files.cdn %>"
-		},
-		themes: {
-			src: [
-				"AUTHORS.txt",
-				"MIT-LICENSE.txt",
-				"package.json"
-			],
-			dest: "dist/<%= files.themes %>"
-		}
-	},
-	zip: {
-		dist: {
-			src: "<%= files.dist %>",
-			dest: "<%= files.dist %>.zip"
-		},
-		cdn: {
-			src: "<%= files.cdn %>",
-			dest: "<%= files.cdn %>.zip"
-		},
-		themes: {
-			src: "<%= files.themes %>",
-			dest: "<%= files.themes %>.zip"
-		}
-	},
-	md5: {
-		dist: {
-			src: "dist/<%= files.dist %>",
-			dest: "dist/<%= files.dist %>/MANIFEST"
-		},
-		cdn: {
-			src: "dist/<%= files.cdn %>",
-			dest: "dist/<%= files.cdn %>/MANIFEST"
-		},
-		themes: {
-			src: "dist/<%= files.themes %>",
-			dest: "dist/<%= files.themes %>/MANIFEST"
-		}
-	},
 	qunit: {
-		files: grunt.file.expandFiles( "tests/unit/**/*.html" ).filter(function( file ) {
+		files: expandFiles( "tests/unit/**/*.html" ).filter(function( file ) {
 			// disabling everything that doesn't (quite) work with PhantomJS for now
 			// TODO except for all|index|test, try to include more as we go
-			return !( /(all|index|test|dialog|dialog_deprecated|datepicker|timepicker|tabs|tooltip|draggable)\.html$/ ).test( file );
+			return !( /(all|index|test|dialog|dialog_deprecated|datepicker|timepicker|tooltip|draggable)\.html$/ ).test( file );
 		})
-	},
-	lint: {
-		ui: "ui/*.js",
-		grunt: [ "grunt.js", "build/**/*.js" ],
-		tests: "tests/unit/**/*.js"
 	},
 	jshint: {
 		options: {
@@ -344,11 +226,7 @@ grunt.initConfig({
 grunt.registerTask( "default", [ "lint", "test" ]);
 grunt.registerTask( "lint", [ "asciilint", "jshint", "jscs", "csslint", "htmllint" ]);
 grunt.registerTask( "test", [ "qunit" ]);
-grunt.registerTask( "sizer", "concat:ui min:dist/jquery-ui.min.js compare_size:all" );
-grunt.registerTask( "sizer_all", "concat:ui min compare_size" );
-grunt.registerTask( "build", "concat min cssmin copy:dist_units_images" );
-grunt.registerTask( "release", "clean build copy:dist copy:dist_min copy:dist_min_images copy:dist_css_min md5:dist zip:dist" );
-grunt.registerTask( "release_themes", "release generate_themes copy:themes md5:themes zip:themes" );
-grunt.registerTask( "release_cdn", "release_themes copy:cdn copy:cdn_min copy:cdn_i18n copy:cdn_i18n_min copy:cdn_min_images copy:cdn_themes md5:cdn zip:cdn" );
+grunt.registerTask( "sizer", [ "concat:ui", "uglify:main", "compare_size:all" ]);
+grunt.registerTask( "sizer_all", [ "concat:ui", "uglify", "compare_size" ]);
 
 };
